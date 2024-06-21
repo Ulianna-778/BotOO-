9477907bb3f159b3f102b5b90753cd48f58b928b
Ulianna-778 <ulianna788@mail.ru>
2024-06-21 19:57:22 +0300

    Telegram Bot nnnnnnnnnnn

---
+++ src/main/java/User.java
@@ -0,0 +1,49 @@
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+    @Id
+    private Long id;
+    @Column(name = "user_name")
+    private String userName;
+    @Column(name = "first_name")
+    private String firstName;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userName='" + userName + '\'' +
+                ", id=" + id +
+                ", firstName='" + firstName + '\'' +
+                '}';
+    }
+}
